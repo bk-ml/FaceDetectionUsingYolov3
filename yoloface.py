d30a401db5f1171352d57b9b310a7b3a94c49977
BhawnaKochhar <60460787+BhawnaKochhar@users.noreply.github.com>
2020-10-05 19:10:13 +0530

    Update yoloface.py

--- yoloface.py
+++ yoloface.py
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--model-cfg', type=str, default='./cfg/yolov3-face.cfg',
                     help='path to config file')
 parser.add_argument('--model-weights', type=str,
-                    default='yolov3.weights',
+                    default='weight/yolov3.weights',
                     help='path to weights of model')
 parser.add_argument('--image', type=str, default='',
                     help='path to image file')
